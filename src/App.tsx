c129426ccc61c209788179e5711d1391f3497140
aaade-pm <Olatunjiademolaimmanuel@gmail.com>
2025-02-26 09:17:47 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="bg-white text-black h-screen font-bold">Utica Website</div>
+  );
+};
+
+export default App;
